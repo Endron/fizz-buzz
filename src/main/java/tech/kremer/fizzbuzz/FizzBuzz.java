4997fe658365857720fb00f5840c5dce91c8ff11
Tobias Kremer <t.kremer@reply.de>
2019-07-12 11:21:20 +0200

    add FizzBuzz class

---
+++ src/main/java/tech/kremer/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,4 @@
+package tech.kremer.fizzbuzz;
+
+public class FizzBuzz {
+}
---
+++ src/test/java/tech/kremer/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,6 @@
+package tech.kremer.fizzbuzz;
+
+public class FizzBuzzTest {
+
+    private final FizzBuzz fizzBuzz = new FizzBuzz();
+}
